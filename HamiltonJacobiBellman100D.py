2f3d0f213f18b1802c3a947c13597bdda1f1f3fa
dpnshvrm <64657500+dpnshvrm@users.noreply.github.com>
2022-09-28 11:27:30 -0400

    updated files

--- HamiltonJacobiBellman100D.py
+++ HamiltonJacobiBellman100D.py
@@ -1,40 +1,146 @@
-"""
-@author: Maziar Raissi
-"""
-
 import numpy as np
 import tensorflow as tf
-from FBSNNs import FBSNN
+from Neural_SOC import NeuralSOC
 import matplotlib.pyplot as plt
-from plotting import newfig, savefig
+tf.compat.v1.disable_eager_execution()
+import csv, time, gc
+import pickle as pl
 
-class HamiltonJacobiBellman(FBSNN):
+class HamiltonJacobiBellman(NeuralSOC):
     def __init__(self, Xi, T,
                        M, N, D,
-                       layers):
+                       layers, sigma_val, ctrl_dyn, oc_loss, alpha, betas, shift_targ = 0):
+        # alpha: 0 sets oc cost weight to zero
+        # oc_cost_weight: weight to the oc cost
+        # ctrl_dyn: 1 or 0 to have control in the dynamics or not 
         
+        self.x_target  = 0*np.ones([1,D])
+        if shift_targ:
+            self.x_target  = 2*np.ones([1,D])
+        self.sigma_val = sigma_val
+        self.ctrl_dyn = ctrl_dyn
+        self.oc_loss = oc_loss
+        self.file2store = './history/history100D_ctrl-'+str(ctrl_dyn) + '_alpha-' + str(alpha) + '_shift_targ' + str(shift_targ) +'_date-' + time.strftime("%m-%d") + '_time-' + time.strftime("%H-%M") + '.csv'
+
+
+                
         super().__init__(Xi, T,
                          M, N, D,
-                         layers)
-    
+                         layers, alpha, betas)
+        
+        
     def phi_tf(self, t, X, Y, Z): # M x 1, M x D, M x 1, M x D
-        return tf.reduce_sum(Z**2, 1, keepdims = True) # M x 1
+        if self.ctrl_dyn:    
+            return -tf.reduce_sum(Z**2, 1, keepdims = True) # M x 1, return zero f, for non zero control
+        elif  self.ctrl_dyn!=1:
+            return tf.reduce_sum(input_tensor=Z**2, axis=1, keepdims = True) # M x 1, for zero control
     
     def g_tf(self, X): # M x D
-        return tf.log(0.5 + 0.5*tf.reduce_sum(X**2, 1, keepdims = True)) # M x 1
+        return tf.math.log(0.5 + 0.5*tf.reduce_sum(input_tensor=(X - self.x_target)**2, axis=1, keepdims = True)) # M x 1
 
     def mu_tf(self, t, X, Y, Z): # M x 1, M x D, M x 1, M x D
-        return super().mu_tf(t, X, Y, Z) # M x D
+        if self.ctrl_dyn:    
+            return -2*Z # M x D, non zero mu (control)
+        elif  self.ctrl_dyn!=1:
+             return super().mu_tf_zero(t, X, Y, Z) # M x D, zero mu (control)
+        
     
     def sigma_tf(self, t, X, Y): # M x 1, M x D, M x 1
-        return tf.sqrt(2.0)*super().sigma_tf(t, X, Y) # M x D x D
+        return self.sigma_val*super().sigma_tf(t, X, Y) # M x D x D
     
+    def phi_tf_zero(self, t, X, Y, Z): # M x 1, M x D, M x 1, M x 
+        return tf.reduce_sum(input_tensor=Z**2, axis=1, keepdims = True) # M x 1, for zero control
+    
+    def mu_tf_zero(self, t, X, Y, Z): # M x 1, M x D, M x 1, M x D
+        return super().mu_tf_zero(t, X, Y, Z) # M x D, zero mu (control)
     ###########################################################################
+
+# Define exact solution 
+def g(X, X_targ): # MC x NC x D
+        return np.log(0.5 + 0.5*np.sum((X - X_targ)**2, axis=2, keepdims=True)) # MC x N x 1
+        
+def u_exact(t, X, X_targ): # NC x 1, NC x D
+    MC = 10**5
+    NC = t.shape[0]
+    
+    np.random.seed(0)
+    W = np.random.normal(size=(MC,NC,D)) # MC x NC x D
+    
+    return -np.log(np.mean(np.exp(-g(X + np.sqrt(2.0*np.abs(T-t))*W, X_targ)),axis=0))
+
+# get learned solution and relative error corresponding to different number of iterations in 1st dim
+def get_Phi(model, numiter, legnd, samples=5, save_sol = 0):
+    cur_date = time.strftime("%m-%d-%H-%M-%S")
+    t_test, W_test = model.fetch_minibatch()
+
+    X_pred, Y_pred, his = model.predict(Xi, t_test, W_test)
+    Y_test = u_exact(t_test[0, :, :], X_pred[0, :, :], model.x_target)
+
+    Y_test_terminal = np.log(0.5 + 0.5 * np.sum((X_pred[:, -1, :] - model.x_target) ** 2, axis=1, keepdims=True))
+
+    Y_true= Y_test
+    errors = np.reshape(np.sqrt((Y_test - Y_pred[0, :, :]) ** 2 / Y_test ** 2), [-1, 1])
+    for i in range(1, samples):
+        Y_test = u_exact(t_test[i, :, :], X_pred[i, :, :], model.x_target)
+        Y_true = np.append(Y_true,Y_test, axis = 1)
+        errors = np.append(errors, np.reshape(np.sqrt((Y_test - Y_pred[i, :, :]) ** 2 / Y_test ** 2), [-1, 1]), axis=1)
+
+    if save_sol != 1:
+        return t_test, Y_pred, Y_true, Y_test_terminal, errors
+    else:
+        np.savez('./sol_files/sol_relerr-' + legnd + str(numiter) + "-" + cur_date, t = t_test[:samples,:,0], Y_true = Y_true,
+                 Y_true_T = Y_test_terminal[:samples,0], Y_pred = Y_pred[:samples,:,0], rel_err = errors,
+                 oc_loss = np.array(his[0]))
+
+
+# plot learned solution corresponding to different number of iterations
+def plotting(model, numiter, datafile):
+    cur_date, cur_time = time.strftime("%m-%d"), time.strftime("%H-%M")
+    data = np.load(datafile)
+    t_test, Y_test, Y_test_terminal, Y_pred, errors, legnd = data['t'], data['Y_true'].T,\
+                                                      data['Y_true_T'], data['Y_pred'], data['rel_err'], data['legnd']
+    samples = t_test.shape[0]
+    for i in range(samples):
+        plt.figure()
+        plt.plot(t_test[i,:],Y_pred[i,:],'b',label='Learned u(t,Xt)')
+        # plt.plot(t_test[1:5,:,0].T,Y_pred[1:5,:,0].T,'b')
+        plt.plot(t_test[i,:],Y_test[i,:],'r--',label='Exact u(t,Xt)')
+        plt.plot(t_test[i,-1],Y_test_terminal[i],'ks',label='YT = u(T,XT)')
+        # plt.plot(t_test[1:5,-1,0],Y_test_terminal[1:5,0])
+        plt.plot([0],Y_test[i,0],'ko',label='Y0 = u(0,X0)')
+        plt.xlabel('t')
+        plt.ylabel('Yt = u(t,Xt)')
+        plt.title('100-dimensional Hamilton-Jacobi-Bellman')
+        plt.legend()
+        file2savefig = './figures/comparision/HJB_ctrl-'+str(model.ctrl_dyn) + '_alpha-' + str(model.alpha) + '_shift_targ' + str(shift_targ) +'_date-' + cur_date + '_time-' + cur_time + '_iter-' + str(numiter) + '_i-' + str(i) + '.png'
+        plt.savefig(file2savefig , dpi = 100)
+
+    mean_err = np.mean(errors, 1)
+    std_err = np.std(errors, 1)
+    plt.figure()
+    plt.plot(t_test[0,:,0],mean_err,'b')
+    plt.fill_between(t_test[0,:,0], mean_err-std_err, mean_err+std_err, alpha = 0.5)
+    # plt.plot(t_test[0,:,0],errors,'b')
+    plt.xlabel('t')
+    plt.ylabel('relative error')
+    plt.title('100-dimensional Hamilton-Jacobi-Bellman')
+    # plt.legend()
+    file2savefig = './figures/comparision/HJB_rel-error_ctrl-'+str(model.ctrl_dyn) + '_alpha-' + str(model.alpha)+ '_shift_targ' + str(shift_targ)+'_date-' + cur_date + '_time-' + cur_time + '_iter-' + str(numiter)  + '.png'
+    plt.savefig(file2savefig , dpi = 100)
+
+   
 
 
 if __name__ == "__main__":
     
-    M = 100 # number of trajectories (batch size)
+    # set seed 
+    # tf.compat.v1.set_random_seed(1)
+    # tf.random.set_seed(1)
+    #
+    legnd = ['ours', 'FBSNNs', 'no-ocloss']
+    
+    plt.close('all')
+    M = 64 # number of trajectories (batch size)
     N = 50 # number of time snapshots
     D = 100 # number of dimensions
     
@@ -42,58 +148,62 @@ if __name__ == "__main__":
 
     Xi = np.zeros([1,D])
     T = 1.0
-         
-    # Training
-    model = HamiltonJacobiBellman(Xi, T,
+    sigma_val = tf.sqrt(2.0)
+     
+    # shifted target problem
+    shift_targ = 0
+    # to add oc cost and weight in the loss function
+    alpha = 20.0
+    betas = [1.0, 1.0, 1.0]
+    n_iter = [2*10**4, 3*10**4, 3*10**4, 2*10**4]
+    model_ours = HamiltonJacobiBellman(Xi, T,
+                                       M, N, D,
+                                       layers, sigma_val, True, True, alpha, betas, shift_targ = shift_targ)
+    
+
+    model_ours.train(N_Iter=n_iter[0], learning_rate=1e-3)
+    get_Phi(model_ours, n_iter[0], legnd[0], samples=10, save_sol=1)
+    model_ours.train(N_Iter=n_iter[1], learning_rate=1e-4)
+    get_Phi(model_ours, n_iter[1], legnd[0], samples=10, save_sol=1)
+    model_ours.train(N_Iter=n_iter[2], learning_rate=1e-5)
+    get_Phi(model_ours, n_iter[2], legnd[0], samples=10, save_sol=1)
+    model_ours.train(N_Iter=n_iter[3], learning_rate=1e-6)
+    get_Phi(model_ours, n_iter[3], legnd[0], samples=10, save_sol=1)
+    
+    del model_ours
+    gc.collect()
+
+    # Training: FBSNNs
+    model_Raissi = HamiltonJacobiBellman(Xi, T,
                                   M, N, D,
-                                  layers)
-        
-    model.train(N_Iter = 2*10**4, learning_rate=1e-3)
-    model.train(N_Iter = 3*10**4, learning_rate=1e-4)
-    model.train(N_Iter = 3*10**4, learning_rate=1e-5)
-    model.train(N_Iter = 2*10**4, learning_rate=1e-6)
+                                  layers, sigma_val, False, False, 0.0, betas, shift_targ = shift_targ)
+
+    model_Raissi.train(N_Iter=n_iter[0], learning_rate=1e-3)
+    get_Phi(model_Raissi, n_iter[0], legnd[1], samples=10, save_sol=1)
+    model_Raissi.train(N_Iter=n_iter[1], learning_rate=1e-4)
+    get_Phi(model_Raissi, n_iter[1], legnd[1], samples=10, save_sol=1)
+    model_Raissi.train(N_Iter=n_iter[2], learning_rate=1e-5)
+    get_Phi(model_Raissi, n_iter[2], legnd[1], samples=10, save_sol=1)
+    model_Raissi.train(N_Iter=n_iter[3], learning_rate=1e-6)
+    get_Phi(model_Raissi, n_iter[3], legnd[1], samples=10, save_sol=1)
+    
+    del model_Raissi
+    gc.collect()
     
     
-    t_test, W_test = model.fetch_minibatch()
+    model_ctrl = HamiltonJacobiBellman(Xi, T,
+                                  M, N, D,
+                                  layers, sigma_val, True, False, 0.0, betas, shift_targ = shift_targ)
+
+    model_ctrl.train(N_Iter=n_iter[0], learning_rate=1e-3)
+    get_Phi(model_ctrl, n_iter[0], legnd[2], samples=10, save_sol=1)
+    model_ctrl.train(N_Iter=n_iter[1], learning_rate=1e-4)
+    get_Phi(model_ctrl, n_iter[1], legnd[2], samples=10, save_sol=1)
+    model_ctrl.train(N_Iter=n_iter[2], learning_rate=1e-5)
+    get_Phi(model_ctrl, n_iter[2], legnd[2], samples=10, save_sol=1)
+    model_ctrl.train(N_Iter=n_iter[3], learning_rate=1e-6)
+    get_Phi(model_ctrl, n_iter[3], legnd[2], samples=10, save_sol=1)
+    del model_ctrl
+    gc.collect()
     
-    X_pred, Y_pred = model.predict(Xi, t_test, W_test)
     
-    def g(X): # MC x NC x D
-        return np.log(0.5 + 0.5*np.sum(X**2, axis=2, keepdims=True)) # MC x N x 1
-        
-    def u_exact(t, X): # NC x 1, NC x D
-        MC = 10**5
-        NC = t.shape[0]
-        
-        W = np.random.normal(size=(MC,NC,D)) # MC x NC x D
-        
-        return -np.log(np.mean(np.exp(-g(X + np.sqrt(2.0*np.abs(T-t))*W)),axis=0))
-    
-    Y_test = u_exact(t_test[0,:,:], X_pred[0,:,:])
-    
-    Y_test_terminal = np.log(0.5 + 0.5*np.sum(X_pred[:,-1,:]**2, axis=1, keepdims=True))
-    
-    plt.figure()
-    plt.plot(t_test[0:1,:,0].T,Y_pred[0:1,:,0].T,'b',label='Learned $u(t,X_t)$')
-    #plt.plot(t_test[1:5,:,0].T,Y_pred[1:5,:,0].T,'b')
-    plt.plot(t_test[0,:,0].T,Y_test[:,0].T,'r--',label='Exact $u(t,X_t)$')
-    plt.plot(t_test[0:1,-1,0],Y_test_terminal[0:1,0],'ks',label='$Y_T = u(T,X_T)$')
-    #plt.plot(t_test[1:5,-1,0],Y_test_terminal[1:5,0])
-    plt.plot([0],Y_test[0,0],'ko',label='$Y_0 = u(0,X_0)$')
-    plt.xlabel('$t$')
-    plt.ylabel('$Y_t = u(t,X_t)$')
-    plt.title('100-dimensional Hamilton-Jacobi-Bellman')
-    plt.legend()
-    
-    # savefig('./figures/HJB_Apr18_50', crop = False)
-    
-    errors = np.sqrt((Y_test-Y_pred[0,:,:])**2/Y_test**2)
-    
-    plt.figure()
-    plt.plot(t_test[0,:,0],errors,'b')
-    plt.xlabel('$t$')
-    plt.ylabel('relative error')
-    plt.title('100-dimensional Hamilton-Jacobi-Bellman')
-    # plt.legend()
-    
-    # savefig('./figures/HJB_Apr18_50_errors', crop = False)
--- plotter.py
+++ plotter.py
@@ -18,24 +18,24 @@ def plot_sol(numiter, datafile1, datafile2, datafile3):
     #                                                   data['Y_true_T'], data['Y_pred'], data['rel_err'], data['legnd']
     samples = data_ours['t'].shape[0]
     plt.figure()      
-    plt.subplot(2,1,1)
+    plt.subplot(2,2,1)
     RE0 = np.linalg.norm(data_ours['Y_pred'][:,0] - 
-                         data_ours['Y_true'].T[:,0])/np.linalg.norm(data_ours['Y_true'].T[:,0])
+                          data_ours['Y_true'].T[:,0])/np.linalg.norm(data_ours['Y_true'].T[:,0])
     
     RE = np.linalg.norm(data_ours['Y_pred'] - 
-                         data_ours['Y_true'].T)/np.linalg.norm(data_ours['Y_true'].T)
+                          data_ours['Y_true'].T)/np.linalg.norm(data_ours['Y_true'].T)
     for i in range(5):  
         plt.plot(data_ours['t'][i,:],data_ours['Y_pred'][i,:],'b',label='Learned u(t,Xt)')
         plt.plot(data_ours['t'][i,:],data_ours['Y_true'].T[i,:],'k--',label='Exact u(t,Xt)')
         plt.xlabel('t')
         plt.ylabel('Yt = u(t,Xt)')
         plt.title('RE = '+str(RE)+',RE0 = '+str(RE0))
-    plt.subplot(2,1,2)
+    plt.subplot(2,2,2)
     RE0 = np.linalg.norm(data_fbsnns['Y_pred'][:,0] - 
-                         data_fbsnns['Y_true'].T[:,0])/np.linalg.norm(data_fbsnns['Y_true'].T[:,0])
+                          data_fbsnns['Y_true'].T[:,0])/np.linalg.norm(data_fbsnns['Y_true'].T[:,0])
     
     RE = np.linalg.norm(data_fbsnns['Y_pred'] - 
-                         data_fbsnns['Y_true'].T)/np.linalg.norm(data_fbsnns['Y_true'].T)
+                          data_fbsnns['Y_true'].T)/np.linalg.norm(data_fbsnns['Y_true'].T)
     for i in range(5):  
         plt.plot(data_ours['t'][i,:],data_fbsnns['Y_pred'][i,:],'r',label='Learned u(t,Xt)-fbsnn')
         plt.plot(data_ours['t'][i,:],data_fbsnns['Y_true'].T[i,:],'k--',label='Exact u(t,Xt)')
@@ -48,54 +48,35 @@ def plot_sol(numiter, datafile1, datafile2, datafile3):
                     top=0.9, 
                     wspace=0.4, 
                     hspace=0.4)
-    file2savefig = './figures/orig/HJB_both_ctrl'+'_date-' + cur_date + '_time-' + cur_time + '_iter-' + str(numiter) 
+    # file2savefig = './figures/orig/HJB_both_ctrl'+'_date-' + cur_date + '_time-' + cur_time + '_iter-' + str(numiter) 
+    # tikzplotlib.save(file2savefig + '.tex')
+    plt.subplot(2,2,3)
+    RE0 = np.linalg.norm(data_noocloss['Y_pred'][:,0] - 
+                         data_noocloss['Y_true'].T[:,0])/np.linalg.norm(data_noocloss['Y_true'].T[:,0])
+    
+    RE = np.linalg.norm(data_noocloss['Y_pred'] - 
+                         data_noocloss['Y_true'].T)/np.linalg.norm(data_noocloss['Y_true'].T)
+    for i in range(5):  
+        plt.plot(data_ours['t'][i,:],data_noocloss['Y_pred'][i,:],'r',label='Learned u(t,Xt)-nooc')
+        plt.plot(data_ours['t'][i,:],data_noocloss['Y_true'].T[i,:],'k--',label='Exact u(t,Xt)')
+        plt.xlabel('t')
+        plt.ylabel('Yt = u(t,Xt)')
+        plt.title('RE = '+str(RE)+',RE0 = '+str(RE0))
+    plt.subplots_adjust(left=0.1,
+                    bottom=0.001, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+    file2savefig = './figures/comparision/HJB_all_ctrl'+'_date-' + cur_date + '_time-' + cur_time + '_iter-' + str(numiter) 
     tikzplotlib.save(file2savefig + '.tex')
 
-data_ours = 'July_15/sol_relerr-ours20000-07-15-11-42-29.npz'    
-data_fbsnns = 'July_15/sol_relerr-FBSNNs20000-07-15-18-15-06.npz'
-data_noocloss = 'July_15/sol_relerr-no-ocloss20000-07-15-20-55-19.npz'
+data_ours = 'sol_relerr-ours20000-07-15-11-42-29.npz'    
+data_fbsnns = 'sol_relerr-FBSNNs20000-07-15-18-15-06.npz'
+data_noocloss = 'sol_relerr-no-ocloss20000-07-15-20-55-19.npz'
 plot_sol(0, data_ours, data_fbsnns, data_noocloss)
 
-data_ours = 'July_15/sol_relerr-ours30000-07-15-14-01-57.npz'    
-data_fbsnns = 'July_15/sol_relerr-FBSNNs30000-07-15-18-40-11.npz'
-data_noocloss = 'July_15/sol_relerr-no-ocloss30000-07-15-23-10-17.npz'
+data_ours = 'sol_relerr-ours30000-07-15-14-01-57.npz'    
+data_fbsnns = 'sol_relerr-FBSNNs30000-07-15-18-40-11.npz'
+data_noocloss = 'sol_relerr-no-ocloss30000-07-15-23-10-17.npz'
 plot_sol(1, data_ours, data_fbsnns, data_noocloss)
-
-# data_ours = 'July_15/sol_relerr-ours30000-07-15-16-21-39.npz'
-# data_fbsnns = 'July_15/sol_relerr-FBSNNs30000-07-15-19-05-16.npz'
-# data_noocloss = 'July_15/sol_relerr-no-ocloss30000-07-16-01-25-08.npz'
-# plot_sol(2, data_ours, data_fbsnns, data_noocloss)
-
-# data_ours = 'July_15/sol_relerr-ours20000-07-15-17-56-11.npz'
-# data_fbsnns = 'July_15/sol_relerr-FBSNNs20000-07-15-19-23-29.npz'
-# data_noocloss = 'July_15/sol_relerr-no-ocloss20000-07-16-02-56-24.npz'
-# plot_sol(3, data_ours, data_fbsnns, data_noocloss)
-
-# his_ours = genfromtxt('./sol_files/July_15/history/history100D_ctrl-True_alpha-20.0_shift_targ0_date-07-15_time-10-12.csv', delimiter=',')
-# his_fbsnns = genfromtxt('./sol_files/July_15/history/history100D_ctrl-False_alpha-0.0_shift_targ0_date-07-15_time-18-00.csv', delimiter=',')
-# his_nooloss = genfromtxt('./sol_files/July_15/history/history100D_ctrl-True_alpha-0.0_shift_targ0_date-07-15_time-19-27.csv', delimiter=',')
-
-# for i in range(4):
-#     plt.figure()
-#     if i==0:
-#         plt.semilogy(his_ours[0:200,0],his_ours[0:200,3],'b',label='ours')
-#         plt.semilogy(his_ours[0:200,0],his_fbsnns[0:200,3],'r',label='fbsnn')
-#         plt.semilogy(his_ours[0:200,0],his_nooloss[0:200,3],'m',label='no-ocloss')
-#     elif i==1:
-#         plt.semilogy(his_ours[200:500,0],his_ours[200:500,3],'b',label='ours')
-#         plt.semilogy(his_ours[200:500,0],his_fbsnns[200:500,3],'r',label='fbsnn')
-#         plt.semilogy(his_ours[200:500,0],his_nooloss[200:500,3],'m',label='no-ocloss')
-#     elif i==2:
-#         plt.semilogy(his_ours[500:800,0],his_ours[500:800,3],'b',label='ours')
-#         plt.semilogy(his_ours[500:800,0],his_fbsnns[500:800,3],'r',label='fbsnn')
-#         plt.semilogy(his_ours[500:800,0],his_nooloss[500:800,3],'m',label='no-ocloss')
-#     elif i==3:
-#         plt.semilogy(his_ours[800:1000,0],his_ours[800:1000,3],'b',label='ours')
-#         plt.semilogy(his_ours[800:1000,0],his_fbsnns[800:1000,3],'r',label='fbsnn')
-#         plt.semilogy(his_ours[800:1000,0],his_nooloss[800:1000,3],'m',label='no-ocloss')
-            
-#     plt.xlabel('t')
-#     plt.ylabel('J')
-#     plt.title('OC cost for 100-dimensional Hamilton-Jacobi-Bellman')
-#     plt.legend()
-#     plt.show()
